9546cbfcbfd27e42be50be319d670af13df06ed7
Samuel Corsi-House <chouse.samuel@gmail.com>
2023-11-22 22:20:50 -0500

    feat: improved intervals

--- main.js
+++ main.js
@@ -28,31 +28,53 @@ document.querySelector(
   "#multiplier"
 ).innerText = `Multiplier: ${multiplier.toFixed(2)}`;
 
+const improvedInterval = (cb, tickFn) => {
+  const tick = tickFn();
+  const interval = setInterval(() => {
+    if (tickFn() !== tick) {
+      clearInterval(interval);
+      if (tickFn() !== null) improvedInterval(cb, tickFn);
+    } else cb();
+  }, tick);
+};
+
+const tickSpeed = () =>
+  document.querySelector("#tickSpeedMultiplier").value === ""
+    ? 1
+    : parseFloat(document.querySelector("#tickSpeedMultiplier").value, 10) <= 0
+    ? 1
+    : parseFloat(document.querySelector("#tickSpeedMultiplier").value, 10);
+
 let ready = false;
-setInterval(() => {
-  if (ready) return;
+improvedInterval(
+  () => {
+    if (ready) return;
 
-  const scale = d3
-    .scaleLinear()
-    .domain([0, it])
-    .range([0, 2 * Math.PI]);
+    const scale = d3
+      .scaleLinear()
+      .domain([0, it])
+      .range([0, 2 * Math.PI]);
 
-  g.selectAll("line").remove();
-  g.selectAll("line")
-    .data(new Array(it).fill(0).map((_, i) => i))
-    .enter()
-    .append("line")
-    .attr("stroke", "white")
-    .attr("x1", (d) => radius * Math.cos(scale(d)))
-    .attr("y1", (d) => radius * Math.sin(scale(d)))
-    .attr("x2", (d) => radius * Math.cos(scale(d * multiplier)))
-    .attr("y2", (d) => radius * Math.sin(scale(d * multiplier)));
+    g.selectAll("line").remove();
+    g.selectAll("line")
+      .data(new Array(it).fill(0).map((_, i) => i))
+      .enter()
+      .append("line")
+      .attr("stroke", "white")
+      .attr("x1", (d) => radius * Math.cos(scale(d)))
+      .attr("y1", (d) => radius * Math.sin(scale(d)))
+      .attr("x2", (d) => radius * Math.cos(scale(d * multiplier)))
+      .attr("y2", (d) => radius * Math.sin(scale(d * multiplier)));
 
-  if (it === divisions - 1) ready = true;
-  it++;
+    if (it === divisions) ready = true;
+    else it++;
 
-  document.querySelector("#divisions").innerText = `Divisions: ${it}`;
-}, 20);
+    document.querySelector("#divisions").innerText = `Divisions: ${it}`;
+  },
+  () =>
+    console.log(tickSpeed()) ||
+    (it > divisions ? null : it >= divisions / 3 ? 20 : 50) / tickSpeed()
+);
 
 const scale = d3
   .scaleLinear()
@@ -60,34 +82,37 @@ const scale = d3
   .range([0, 2 * Math.PI]);
 
 let reversing = false;
-setInterval(() => {
-  if (!ready) return;
+improvedInterval(
+  () => {
+    if (!ready) return;
 
-  g.selectAll("line")
-    .transition()
-    .duration(50)
-    .attr("x1", (d) => radius * Math.cos(scale(d)))
-    .attr("y1", (d) => radius * Math.sin(scale(d)))
-    .attr("x2", (d) => radius * Math.cos(scale(d * multiplier)))
-    .attr("y2", (d) => radius * Math.sin(scale(d * multiplier)))
-    .attr("stroke", "white");
+    g.selectAll("line")
+      .transition()
+      .duration(50)
+      .attr("x1", (d) => radius * Math.cos(scale(d)))
+      .attr("y1", (d) => radius * Math.sin(scale(d)))
+      .attr("x2", (d) => radius * Math.cos(scale(d * multiplier)))
+      .attr("y2", (d) => radius * Math.sin(scale(d * multiplier)))
+      .attr("stroke", "white");
 
-  const allowReversing = document.querySelector("#allowReversing").checked;
-  if (reversing && allowReversing) {
-    if (multiplier <= 8.5) {
-      reversing = false;
+    const allowReversing = document.querySelector("#allowReversing").checked;
+    if (reversing && allowReversing) {
+      if (multiplier <= 8.5) {
+        reversing = false;
+      } else {
+        multiplier -= 0.01;
+      }
     } else {
-      multiplier -= 0.01;
+      if (multiplier >= 12 && allowReversing) {
+        reversing = true;
+      } else {
+        multiplier += 0.01;
+      }
     }
-  } else {
-    if (multiplier >= 10 && allowReversing) {
-      reversing = true;
-    } else {
-      multiplier += 0.01;
-    }
-  }
 
-  document.querySelector(
-    "#multiplier"
-  ).innerText = `Multiplier: ${multiplier.toFixed(2)}`;
-}, 50);
+    document.querySelector(
+      "#multiplier"
+    ).innerText = `Multiplier: ${multiplier.toFixed(2)}`;
+  },
+  () => 50 / tickSpeed()
+);
